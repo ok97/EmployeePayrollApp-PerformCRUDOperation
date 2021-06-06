9f9fb7496e471f744729192ce6ec0abd6448d6a4
ok97 <omkhawshi0@gmail.com>
2021-06-06 15:54:28 +0530

    Added: Update the Local Storage with the Employee Payroll t

--- js/new_home.js
+++ js/new_home.js
@@ -61,3 +61,11 @@ const remove = (node) => {
   createInnerHtml();
 }
 
+// Update a particular employee details
+
+const update = (node) => {
+  let empPayrollData = empPayrollList.find(empData => empData._id == node.id);
+  if (!empPayrollData) return;
+  localStorage.setItem('editEmp', JSON.stringify(empPayrollData))
+  window.location.replace(site_properties.add_emp_payroll_page);
+}
